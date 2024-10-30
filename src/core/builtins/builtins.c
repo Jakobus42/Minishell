c22d72441f194598a5c9921eb52fd016f997e6ba
Jakobus42 <Jakob.sadjina@icloud.com>
2024-10-30 11:46:18 +0000

    fix: segfault if getcwd fails

--- src/core/builtins/builtins.c
+++ src/core/builtins/builtins.c
@@ -54,7 +54,8 @@ void which_builtin(t_shell *shell, t_command *cmd)
 	else if (!ft_strcmp(cmd->args[0], "pwd"))
 	{
 		temp = getcwd(NULL, 0);
-		ft_putendl_fd(temp, 1);
+		if (temp)
+			ft_putendl_fd(temp, 1);
 		free(temp);
 	}
 	else if (!ft_strcmp(cmd->args[0], "export") && !cmd->args[1])
